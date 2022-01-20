2a8830b25518de861f8d43ae158d4b809934f5ee
Adarsh Gupta <69007272+adarsh1907@users.noreply.github.com>
2022-01-20 18:00:27 +0530

    Assignment 1

--- stopwatch.py
+++ stopwatch.py
@@ -4,7 +4,6 @@ import time
 hours,minutes,seconds = 0,0,0
 while(True):
   print(hours,":",minutes,":",seconds)
-  time.sleep(1)
   seconds+=1
   if(seconds==60):
     seconds=0
@@ -12,4 +11,6 @@ while(True):
   if(minutes==60):
     minutes=0
     hours+=1
-  os.system('cls')
+  time.sleep(1)
+
+os.system('cls')
